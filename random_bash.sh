893aa86218d5518ade09b0d48e50657fc8a3313b
Dmitry Babitsky <dbabits@users.noreply.github.com>
2017-12-21 08:58:52 -0500

    Update random_bash.sh

--- random_bash.sh
+++ random_bash.sh
@@ -5,3 +5,22 @@ echo 'foo moo' 2>&1 |grep -v bar|awk '{print $2}'
 echo retcode=${PIPESTATUS[0]}
 EOF
 )
+
+#get string between tokens, using grep
+> echo "Here is a string is a string" | grep -o -P '(?<=Here).*(?=string)'
+is a string is a
+
+> echo "Here is a string is a string" | grep -o -P '(?<=Here).*?(?=string)'
+is a
+
+#Sort, except header:
+echo '
+header
+line2
+line1
+'|awk 'NR<3{print $0;next}{print $0| "sort "}'
+
+header
+
+line1
+line2
