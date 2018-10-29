48b9df2e3cd243d295dd228c6439a5fadf07ad45
Dmitry Babitsky <dbabits@users.noreply.github.com>
2018-10-29 17:03:23 -0400

    jq job counters

--- jq.sh
+++ jq.sh
@@ -93,3 +93,20 @@ $ echo "$json" | jq '..|select(.first_name != null and .last_name !=null)?|[.fir
 #If your jq does not have walk/1, then you can copy its definition from https://github.com/stedolan/jq/blob/master/src/builtin.jq
 walk(if type == "object" and .first_name and .last_name then .full_name=.first_name+"-"+.last_name else . end)
 walk(if type == "object" and .first_name and .last_name then .first_name=.first_name+"-"+.last_name |.last_name=.first_name+"-"+.last_name else . end)
+
+
+#Combine attributes at different levels of hierarchy, and make an csv(see also sqlite/pivot table):
+cat <<EOF
+{"jobCounters":{"id":"job_1540515215370_2592","counterGroup":[{"counterGroupName":"org.apache.hadoop.mapreduce.FileSystemCounter","counter":[{"name":"FILE_BYTES_READ","totalCounterValue":0,"mapCounterValue":0,"reduceCounterValue":0},{"name":"FILE_BYTES_WRITTEN","totalCounterValue":1138885635,"mapCounterValue":1138616773,"reduceCounterValue":268862},{"name":"FILE_READ_OPS","totalCounterValue":0,"mapCounterValue":0,"reduceCounterValue":0},{"name":"FILE_LARGE_READ_OPS","totalCounterValue":0,"mapCounterValue":0,"reduceCounterValue":0},{"name":"FILE_WRITE_OPS","totalCounterValue":0,"mapCounterValue":0,"reduceCounterValue":0},{"name":"HDFS_BYTES_READ","totalCounterValue":2642139400,"mapCounterValue":2642139400,"reduceCounterValue":0},{"name":"HDFS_BYTES_WRITTEN","totalCounterValue":0,"mapCounterValue":0,"reduceCounterValue":0},{"name":"HDFS_READ_OPS","totalCounterValue":21090,"mapCounterValue":21090,"reduceCounterValue":0},{"name":"HDFS_LARGE_READ_OPS","totalCounterValue":0,"mapCounterValue":0,"reduceCounterValue":0},{"name":"HDFS_WRITE_OPS","totalCounterValue":0,"mapCounterValue":0,"reduceCounterValue":0}]},{"counterGroupName":"org.apache.hadoop.mapreduce.JobCounter","counter":[{"name":"NUM_KILLED_MAPS","totalCounterValue":3800,"mapCounterValue":0,"reduceCounterValue":0},{"name":"TOTAL_LAUNCHED_MAPS","totalCounterValue":8028,"mapCounterValue":0,"reduceCounterValue":0},{"name":"TOTAL_LAUNCHED_REDUCES","totalCounterValue":1,"mapCounterValue":0,"reduceCounterValue":0},{"name":"DATA_LOCAL_MAPS","totalCounterValue":5954,"mapCounterValue":0,"reduceCounterValue":0},{"name":"RACK_LOCAL_MAPS","totalCounterValue":2074,"mapCounterValue":0,"reduceCounterValue":0},{"name":"SLOTS_MILLIS_MAPS","totalCounterValue":156885471756,"mapCounterValue":0,"reduceCounterValue":0},{"name":"MILLIS_MAPS","totalCounterValue":13073789313,"mapCounterValue":0,"reduceCounterValue":0},{"name":"VCORES_MILLIS_MAPS","totalCounterValue":13073789313,"mapCounterValue":0,"reduceCounterValue":0},{"name":"MB_MILLIS_MAPS","totalCounterValue":160650723078144,"mapCounterValue":0,"reduceCounterValue":0}]}]}}
+EOF
+
+#WRONG:
+{ id: .jobCounters.id, name: .jobCounters.counterGroup[].counter[].name, mapCounterValue: .jobCounters.counterGroup[].counter[].mapCounterValue, reduceCounterValue: .jobCounters.counterGroup[].counter[].reduceCounterValue, total: .jobCounters.counterGroup[].counter[].totalCounterValue}  | [.id, .name, .mapCounterValue,.reduceCounterValue,.total] |@csv
+"job_1540515215370_2592","FILE_BYTES_READ",0,0,0
+"job_1540515215370_2592","FILE_BYTES_READ",0,0,1138885635
+
+#RIGHT:
+ .jobCounters.counterGroup[].counter[]  | [.name, .mapCounterValue, .reduceCounterValue, .totalCounterValue] | @csv
+"FILE_BYTES_READ",0,0,0
+"FILE_BYTES_WRITTEN",1138616773,268862,1138885635
+"FILE_READ_OPS",0,0,0
